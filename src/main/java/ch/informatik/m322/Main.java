2acc48c11478737c917a5a59b7b8dfa826ab5ecf
Noël Jörg <noelgamemc@gmail.com>
2023-10-24 14:34:41 +0200

    test

--- src/main/java/ch/informatik/m322/Main.java
+++ src/main/java/ch/informatik/m322/Main.java
@@ -3,6 +3,7 @@ package ch.informatik.m322;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        // Hallo 2.0!
+        // Hallo 2.0! Visual Studio Code
+        // Hallo von IntelliJ
     }
 }
