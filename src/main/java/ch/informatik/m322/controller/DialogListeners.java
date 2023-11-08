ce439c0acc26eaa4f8b42273c01b0e7e370f68ef
marcel dedic <mdedic776@gmail.com>
2023-11-08 13:12:29 +0100

    pom.xml dependency for mariadb jdbc added, javadoc comments added to MainListeners and DialogListeners

--- src/main/java/ch/informatik/m322/controller/DialogListeners.java
+++ src/main/java/ch/informatik/m322/controller/DialogListeners.java
@@ -8,14 +8,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
+/**
+ * Class with ActionListeners for the buttons in the DialogWindow
+ */
 public class DialogListeners {
     private DialogWindow dialogWindow;
     private DialogController dialogController;
 
+    /**
+     * constructor
+     * @param dialogController
+     */
     public DialogListeners(DialogController dialogController) {
         this.dialogController = dialogController;
     }
 
+    /**
+     * setup ActionListener for the cancel-button in DialogWindow
+     * when clicking the button, dialogWindow closes
+     * @param btnCancel
+     */
     public void setupCancel(JButton btnCancel) {
         btnCancel.addActionListener(new ActionListener() {
             @Override
@@ -26,6 +38,13 @@ public class DialogListeners {
 
     }
 
+    /**
+     * setup ActionListener for the create-button in DialogWindow
+     * when the button is clicked, checkValidation() is executed
+     * if checkValidation() returns true, createNewPerson() is executed and dialogWindow disposes
+     * Errors will be handled with try-catch
+     * @param btnCreate
+     */
     public void setupCreate(JButton btnCreate) {
         btnCreate.addActionListener(new ActionListener() {
             @Override
@@ -44,6 +63,13 @@ public class DialogListeners {
         });
     }
 
+    /**
+     * setup ActionListener for the edit-button in DialogWindow
+     * when the button is clicked, checkValidation() is executed
+     * if checkValidation() returns true, updatePerson() is executed and dialogWindow disposes
+     * Errors will be handled with try-catch
+     * @param btnEdit
+     */
     public void setupEdit(JButton btnEdit) {
         btnEdit.addActionListener(new ActionListener() {
             @Override
@@ -60,6 +86,10 @@ public class DialogListeners {
         });
     }
 
+    /**
+     * setup the dialogWindow
+     * @param dialogWindow
+     */
     public void setDialogWindow(DialogWindow dialogWindow) {
         this.dialogWindow = dialogWindow;
     }
--- src/main/java/ch/informatik/m322/controller/MainListeners.java
+++ src/main/java/ch/informatik/m322/controller/MainListeners.java
@@ -14,6 +14,9 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.SQLException;
 
+/**
+ * Class with ActionListeners for necessary components in the MainWindow
+ */
 public class MainListeners {
     private JButton btnSwitchLeft;
     private JButton btnSwitchRight;
@@ -26,6 +29,18 @@ public class MainListeners {
     private MainWindow window;
     private Connector connector;
 
+    /**
+     * constructor to initialise the ActionListeners
+     * @param mainController
+     * @param btnSwitchLeft
+     * @param btnSwitchRight
+     * @param btnCreatePerson
+     * @param btnDeletePerson
+     * @param btnEditPerson
+     * @param personenTable
+     * @param personenTableModel
+     * @param window
+     */
     public MainListeners(MainController mainController, JButton btnSwitchLeft, JButton btnSwitchRight, JButton btnCreatePerson, JButton btnDeletePerson, JButton btnEditPerson, JTable personenTable, DefaultTableModel personenTableModel, MainWindow window) {
         this.mainController = mainController;
         this.btnSwitchLeft = btnSwitchLeft;
@@ -48,6 +63,11 @@ public class MainListeners {
         setupWindowCloseListener();
     }
 
+    /**
+     * setup ActionListener for the switchLeft button in MainWindow
+     * if actionPerformed, index will be set accordingly with setIndex() and updateInfo() aswell as updateTable() is executed.
+     * Errors are handled with try-catch
+     */
     private void setupSwitchLeft() {
         btnSwitchLeft.addActionListener(new ActionListener() {
             @Override
@@ -70,6 +90,11 @@ public class MainListeners {
         });
     }
 
+    /**
+     * setup ActionListener for the switchRight button in MainWindow
+     * if actionPerformed, index will be set accordingly with setIndex() and updateInfo() aswell as updateTable() is executed.
+     * Errors are handled with try-catch
+     */
     private void setupSwitchRight() {
         btnSwitchRight.addActionListener(new ActionListener() {
             @Override
@@ -93,6 +118,10 @@ public class MainListeners {
         });
     }
 
+    /**
+     * setup ActionListener for the create-button in MainWindow
+     * when the button is clicked, setupCreateDialog() is executed
+     */
     private void setupCreatePerson() {
         btnCreatePerson.addActionListener(new ActionListener() {
             @Override
@@ -102,17 +131,23 @@ public class MainListeners {
         });
     }
 
+    /**
+     * setup ActionListener for the edit-button in MainWindow
+     * when the button is clicked, setupEditDialog() is executed with selected Person as parameter
+     */
     private void setupEditPerson() {
         btnEditPerson.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                mainController.getDialogController();
                 mainController.getDialogController().setupEditDialog(mainController.getPerson());
             }
         });
     }
 
-
+    /**
+     * setup ActionListener for the delete-button in MainWindow
+     * when the button is clicked, setupCreateDialog() is executed
+     */
     private void setupDeletePerson() {
         btnDeletePerson.addActionListener(new ActionListener() {
             @Override
@@ -129,6 +164,9 @@ public class MainListeners {
         });
     }
 
+    /**
+     * @TODO
+     */
     private void setupTableSelectionListener() {
         personenTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
@@ -150,6 +188,11 @@ public class MainListeners {
         });
     }
 
+    /**
+     * setup ActionListener for the MainWindow
+     * if MainWindow is closing, close connection to database
+     * Errors are handled with try-catch
+     */
     private void setupWindowCloseListener() {
         window.addWindowListener(new WindowAdapter() {
             @Override
